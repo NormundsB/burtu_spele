1eaacc653141f0cfff4a100edb454606a2d616ca
NormundsB <92847485+NormundsB@users.noreply.github.com>
2022-02-12 20:33:45 +0200

    Update parbaude.js

--- static/js/parbaude.js
+++ static/js/parbaude.js
@@ -1,5 +1,3 @@
 console.log("Sveiks, Norči!!");
 
-document.getElementById('generet').addEventListener('click', () => {
-    generet();
-});
+document.getElementById('generet').addEventListener('click', () => { generet(); });
