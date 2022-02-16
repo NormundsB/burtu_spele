d57f612b320b1c7b5888723b3ddba294a86c9dca
NormundsB <92847485+NormundsB@users.noreply.github.com>
2022-02-16 11:16:17 +0200

    Parbaudes darīšanas

--- static/js/spele.js
+++ static/js/spele.js
@@ -13,53 +13,45 @@ console.log(adrese)
 let vards = "";
 let vards2 = "";
 
-// let vardi4 = ['koks', 'kāja', 'labs', 'lini', 'logs', 'daba', 'dēls', 'egle', 'ezis', 'gads', 'gods', 'govs', 'dēle', "kaza", "maza", "ķeza", "roka", "alus", "māsa", "māte", "tēvs", "kaps", "āzis", "pupa", "odze"];
-// let vardi5 = ['kaķis', 'kakls', 'kalns', 'kauls', 'kļava', 'krūms', 'lācis', 'laiks', 'laime', 'laiva', 'lapsa', 'lauks', 'liepa', 'dzīve', 'ezers', 'šķēle', "žurka", "burka", "biete"];
-// let vardi6 = ['karogs', 'kleita', 'kalējs', 'komats', 'krekls', 'krēsls', 'kurmis', 'kurpes', 'krūtis', 'labība', 'lietus', 'nedēļa', 'laipns', "dibens", "zibens", "klaips"]
-// let vardi7 = ['kadiķis', 'Kurzeme', 'Latgale', 'Vidzeme', 'Zemgale', 'nākotne', 'pasaule', 'pirksts', 'pērkons', 'pilsēta', 'rietumi', 'smiltis', 'sudrabs']
-// let vardi8 = ['kukainis', 'liktenis', 'pīlādzis', 'taurenis', 'uzdevums', 'vecāmāte', 'zīdainis', 'zemnieks', 'zvaigzne', 'folklora', 'dzimtene', 'dzeltens']
-// let vardi9 = ['gliemezis', 'dzirnavas', 'draudzība', 'dzejnieks', 'biezpiens', 'mīlestība', 'patiesība', 'pavasaris', 'skolotājs', 'zvejnieks', 'zvirbulis']
-// let vardi10 = ['kartupelis', 'lakstīgala', 'varavīksne', 'basketbols', 'rakstnieks', 'Lieldienas', 'strēlnieks', 'valodnieks'];
+let vardi4 = ['koks', 'kāja', 'labs', 'lini', 'logs', 'daba', 'dēls', 'egle', 'ezis', 'gads', 'gods', 'govs', 'dēle', "kaza", "maza", "ķeza", "roka", "alus", "māsa", "māte", "tēvs", "kaps", "āzis", "pupa", "odze"];
+let vardi5 = ['kaķis', 'kakls', 'kalns', 'kauls', 'kļava', 'krūms', 'lācis', 'laiks', 'laime', 'laiva', 'lapsa', 'lauks', 'liepa', 'dzīve', 'ezers', 'šķēle', "žurka", "burka", "biete"];
+let vardi6 = ['karogs', 'kleita', 'kalējs', 'komats', 'krekls', 'krēsls', 'kurmis', 'kurpes', 'krūtis', 'labība', 'lietus', 'nedēļa', 'laipns', "dibens", "zibens", "klaips"]
+let vardi7 = ['kadiķis', 'Kurzeme', 'Latgale', 'Vidzeme', 'Zemgale', 'nākotne', 'pasaule', 'pirksts', 'pērkons', 'pilsēta', 'rietumi', 'smiltis', 'sudrabs']
+let vardi8 = ['kukainis', 'liktenis', 'pīlādzis', 'taurenis', 'uzdevums', 'vecāmāte', 'zīdainis', 'zemnieks', 'zvaigzne', 'folklora', 'dzimtene', 'dzeltens']
+let vardi9 = ['gliemezis', 'dzirnavas', 'draudzība', 'dzejnieks', 'biezpiens', 'mīlestība', 'patiesība', 'pavasaris', 'skolotājs', 'zvejnieks', 'zvirbulis']
+let vardi10 = ['kartupelis', 'lakstīgala', 'varavīksne', 'basketbols', 'rakstnieks', 'Lieldienas', 'strēlnieks', 'valodnieks'];
 
 let ieprVards = "";
 let ieprVards2 = "";
 let ieprVards3 = "";
-let failaNosaukums = "";
 let fvardi = "";
+let failaNosaukums = "";
 function generet() {
     let select = document.getElementById('izvele');
     let izvele = select.options[select.selectedIndex].value;
     let vardi = [];
 
-    // if (izvele == 4) {
-    //     vardi = vardi4;
-    // } else if (izvele == 5) {
-    //     vardi = vardi5;
-    // } else if (izvele == 6) {
-    //     vardi = vardi6;
-    // } else if (izvele == 7) {
-    //     vardi = vardi7;
-    // } else if (izvele == 8) {
-    //     vardi = vardi8;
-    // } else if (izvele == 9) {
-    //     vardi = vardi9;
-    // } else if (izvele == 10) {
-    //     vardi = vardi10;
-    // }
-    //Šis ir faila izvelei
+
     if (izvele == 4) {
+        vardi = vardi4;
         fvardi = "vardi4";
     } else if (izvele == 5) {
+        vardi = vardi5;
         fvardi = "vardi5";
     } else if (izvele == 6) {
+        vardi = vardi6;
         fvardi = "vardi6";
     } else if (izvele == 7) {
+        vardi = vardi7;
         fvardi = "vardi7";
     } else if (izvele == 8) {
+        vardi = vardi8;
         fvardi = "vardi8";
     } else if (izvele == 9) {
+        vardi = vardi9;
         fvardi = "vardi9";
     } else if (izvele == 10) {
+        vardi = vardi10;
         fvardi = "vardi10";
     }
 
@@ -68,28 +60,28 @@ function generet() {
 
     // 
 
-    fvardi = "vardi4";
+
     // Šeit ielādējam datus no vardu faila
 
-    let failaNosaukums = "https://normundsb.github.io/burtu_spele/" + fvardi + ".json"
+    failaNosaukums = "https://normundsb.github.io/burtu_spele/" + fvardi + ".json";
 
-    // async function vardPecGaruma() {
-    let datiNoApi = await fetch(failaNosaukums);
-    let datiJson = await datiNoApi.json();
-    console.log(failaNosaukums)
-    console.log(datiJson);
-    // return datiJson
-    // }
-    // async function raditDatus() {
-    //     let jvards = await vardPecGaruma();
-    //     console.log(jvardi);
-    //     for (let i = 0; i < jvards.length; i++) {
-    //         console.log(jvards[i]["title"])
-    //         //     console.log(jvards[1]);
-    //     }
-    // }
+    async function vardPecGaruma() {
+        let datiNoApi = await fetch(failaNosaukums);
+        let datiJson = await datiNoApi.json();
+        // console.log(failaNosaukums)
+        // console.log(datiJson);
+        return datiJson
+    }
+    async function raditDatus() {
+        let jvards = await vardPecGaruma();
+        console.log(jvards);
+        for (let i = 0; i < jvards.length; i++) {
+            console.log(jvards[i])
 
-    // raditDatus();
+        }
+    }
+
+    raditDatus();
 
 
 
@@ -120,7 +112,6 @@ function generet() {
     document.getElementById("vards").innerHTML = vards2;
 
 }
-
 function sajaukt(jVards) {
     var arr = jVards.split('');           // Convert String to array
     var n = arr.length;              // Length of the array
