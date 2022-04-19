baa54598fa85857b23bef728c05d5d1663610d74
NormundsB <92847485+NormundsB@users.noreply.github.com>
2022-04-19 10:58:12 +0300

    script

--- static/js/spele.js
+++ static/js/spele.js
@@ -6,13 +6,13 @@ let niks = adrese[0]
 
 document.querySelector(".virsraksts").innerHTML = niks + " - spēle sākas!"
 
-window.addEventListener('load', function(){
+window.addEventListener('load', function () {
     let pogaGeneret = document.getElementById('generet');
     let select = document.getElementById('izvele');
 
-    pogaGeneret.addEventListener('click', async function(){
-        let izvele = select.options[select.selectedIndex].value;    
-        let saite = "/generet/" + niks + "/" + izvele;        
+    pogaGeneret.addEventListener('click', async function () {
+        let izvele = select.options[select.selectedIndex].value;
+        let saite = "/generet/" + niks + "/" + izvele;
         let atbilde = await fetch(saite);
         let atbildeJson = await atbilde.json()
 
@@ -33,16 +33,54 @@ function spelesLaukums() {
 
 async function parbaudit() {
     let vards = document.getElementById('atbilde').value;
-    let saite = "/parbaudit/" + niks + "/" + vards;   
+    let saite = "/parbaudit/" + niks + "/" + vards;
     let atbilde = await fetch(saite);
     let atbildeJson = await atbilde.json()
-    
+
     alert(atbildeJson.rezultats);
 
-    if(atbildeJson.status == "1"){
+    if (atbildeJson.status == "1") {
         document.getElementById("vards").innerHTML = "";
         document.getElementById("result").innerHTML = "";
         document.getElementById('atbilde').value = "";
     }
 
 }
+
+
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('pulkstenis').innerHTML = h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+    if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
+    return i;
+}
+var i = 0;
+function move() {
+    if (i == 0) {
+        i = 1;
+        var elem = document.getElementById("myBar");
+        var width = 1;
+        var id = setInterval(frame, 360);
+        function frame() {
+            if (width >= 100) {
+                clearInterval(id);
+                i = 0;
+                alert('Tava spēle ir beigusies!');
+            } else {
+                width++;
+                elem.style.width = width + "%";
+                elem.innerHTML = width + "%";
+            }
+        }
+    }
+}
+
