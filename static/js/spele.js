ec155e35a0e59bb1b4693dea48c0f95c8df56864
NormundsB <92847485+NormundsB@users.noreply.github.com>
2022-05-12 09:15:42 +0300

    dati

--- static/js/spele.js
+++ static/js/spele.js
@@ -69,7 +69,7 @@ function move() {
         i = 1;
         var elem = document.getElementById("myBar");
         var width = 1;
-        var id = setInterval(frame, 360);
+        var id = setInterval(frame, 3600);
         function frame() {
             if (width >= 100) {
                 clearInterval(id);
